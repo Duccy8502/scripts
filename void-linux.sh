54d17953c00bdab7f7fd0189b94eb87702dc66ac
jpie726 <53590846+jpie726@users.noreply.github.com>
2021-06-16 18:08:09 -0500

    Update void-linux.sh

--- void-linux.sh
+++ void-linux.sh
@@ -5,4 +5,4 @@ fi
 xbps-install -Su
 xbps-install fish
 adduser user -pmgs resu wheel fish
-xbps-install git pam-devel libxcb-devel base-devel
+xbps-install git pam-devel libxcb-devel base-devel whois bind-utils
